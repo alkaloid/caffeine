1e8365c9eccbb118d4b9002a4c3c391081efc04b
Ben Klang <ben@alkaloid.net>
2017-04-08 12:46:14 -0400

    Remove unused module attributes

--- web/controllers/notify_controller.ex
+++ web/controllers/notify_controller.ex
@@ -2,17 +2,6 @@ defmodule Caffeine.NotifyController do
   use Caffeine.Web, :controller
   require Logger
 
-  @door_camera_map %{
-    1 => %{name: "Central - Front Door", camera_id: 5},
-    2 => %{name: "Central - Side Door", camera_id: 5},
-    3 => %{name: "West Wing - Front Door", camera_id: 6},
-    4 => %{name: "Central - Back Door", camera_id: 3},
-  }
-
-  @camera_url "http://krog.local/cgi-bin/nph-zms?mode=single&monitor="
-  @slack_file_url "https://slack.com/api/files.upload" 
-  @slack_channels "@katharine" # comma separated
-
   def create(conn, %{"target" => target, "message" => message}) do
     send :slack, {:message, message, target}
     conn
